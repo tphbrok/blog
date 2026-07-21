734fe24f48dd5db088815995f53b970cd4d2c889
Thomas Brok <tphbrok@gmail.com>
2026-07-21 21:52:05 +0200

    Rename Blog -> Post, move to post.rs

--- src/main.rs
+++ src/main.rs
@@ -1,73 +1,12 @@
-use std::{
-    collections::HashMap,
-    fs,
-    path::{Path, PathBuf},
-};
+use std::fs;
 
-struct Blog {
-    metadata: HashMap<String, String>,
-    lines: Vec<String>,
-}
+use crate::post::Post;
 
-impl Blog {
-    fn from_path(path: PathBuf) -> Result<Self, Box<dyn std::error::Error>> {
-        let mut lines: Vec<String> = fs::read_to_string(path)?
-            .split("\n")
-            .filter(|line| line.len() > 0)
-            .map(|line| line.to_string())
-            .collect();
-
-        let mut metadata: HashMap<String, String> = HashMap::new();
-
-        let mut metadata_lines = lines.iter().enumerate().filter_map(|(position, line)| {
-            if *line == "---" {
-                return Some(position);
-            }
-
-            None
-        });
-
-        let metadata_start_line_index = metadata_lines.next();
-
-        if let Some(metadata_start_line_index) = metadata_start_line_index {
-            let metadata_end_line_index = metadata_lines
-                .next()
-                .expect("Could not find metadata end (---)");
-
-            for (line_index, line) in lines.iter().enumerate() {
-                if line_index <= metadata_start_line_index {
-                    continue;
-                }
-
-                // If the metadata closer (---) is reached, do not continue
-                if *line == "---" {
-                    break;
-                }
-
-                let (key, value) = line
-                    .split_once(":")
-                    .expect(format!("Badly formatted metadata: {}", line).as_str());
-
-                metadata.insert(key.trim().to_string(), value.trim().to_string());
-            }
-
-            lines = lines
-                .split_at(metadata_end_line_index)
-                .1
-                .to_vec()
-                .split_at_checked(1)
-                .expect("Did not find content after metadata end")
-                .1
-                .to_vec();
-        }
-
-        Ok(Blog { lines, metadata })
-    }
-}
+mod post;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     for blog in fs::read_dir("blogs")? {
-        let blog = Blog::from_path(blog?.path())?;
+        let blog = Post::from_path(blog?.path())?;
 
         dbg!(blog.metadata);
         dbg!(blog.lines);
